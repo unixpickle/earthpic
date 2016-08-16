eab27f1127ac1f7c684d571f129d81614b1b4ff7
Alex Nichol <unixpickle@gmail.com>
2016-08-16 15:01:02 -0400

    added "Center" button to web client

--- web/assets/main.js
+++ web/assets/main.js
@@ -23,10 +23,38 @@
     this.createLight();
     this.registerMouseEvents();
     this.draw();
+
+    var centerButton = document.getElementById('center-button');
+    centerButton.addEventListener('click', this.recenter.bind(this));
   }
 
   App.prototype.draw = function() {
     this.renderer.render(this.scene, this.camera);
+  };
+
+  App.prototype.recenter = function() {
+    var rotAxis = this.rotationAxis();
+    var rotAngle = this.rotationAngle();
+
+    var startMatrix = new THREE.Matrix4();
+    startMatrix.copy(this.mesh.matrix);
+
+    var startTime = new Date().getTime();
+    var updateFunc = function() {
+      var elapsed = (new Date().getTime() - startTime) / 500;
+      if (elapsed > 1 || elapsed < 0) {
+        elapsed = 1;
+      } else {
+        window.requestAnimationFrame(updateFunc);
+      }
+      var rotMatrix = new THREE.Matrix4();
+      rotMatrix.makeRotationAxis(rotAxis, -rotAngle*elapsed);
+      this.mesh.matrix.copy(startMatrix);
+      this.mesh.matrix.premultiply(rotMatrix);
+      this.mesh.rotation.setFromRotationMatrix(this.mesh.matrix);
+      this.draw();
+    }.bind(this);
+    updateFunc();
   };
 
   App.prototype.createGlobe = function() {
@@ -85,6 +113,62 @@
       window.addEventListener('mouseup', upListener);
       window.addEventListener('mousemove', moveListener);
     }.bind(this));
+  };
+
+  App.prototype.rotationAxis = function() {
+    var almostOne = 1 - 1e-5;
+
+    // Find the approximate nullspace of the matrix
+    // (rotation - identity), giving the eigenvector
+    // with eigenvalue 1.
+    var rotation = new THREE.Matrix4();
+    rotation.extractRotation(this.mesh.matrix);
+    rotation.elements[0] -= almostOne;
+    rotation.elements[5] -= almostOne;
+    rotation.elements[10] -= almostOne;
+
+    var invRot = new THREE.Matrix4();
+    invRot.getInverse(rotation);
+    var eigenvector = [Math.random(), Math.random(), Math.random()];
+    invRot.applyToVector3Array(eigenvector);
+
+    var axisVec = new THREE.Vector3(eigenvector[0], eigenvector[1], eigenvector[2]);
+    axisVec.normalize();
+
+    // Make sure the axis vector points the same direction
+    // across successive calls to rotationAxis, since there
+    // are two valid directions it could face.
+    if (axisVec.x < 0) {
+      axisVec.multiplyScalar(-1);
+    }
+
+    return axisVec;
+  };
+
+  App.prototype.rotationAngle = function() {
+    var axis = this.rotationAxis();
+    var orthog = [-axis.y, axis.x, 0];
+
+    var rotation = new THREE.Matrix4();
+    rotation.extractRotation(this.mesh.matrix);
+    rotation.applyToVector3Array(orthog);
+
+    var oldVec = new THREE.Vector3(-axis.y, axis.x, 0);
+    var newVec = new THREE.Vector3(orthog[0], orthog[1], orthog[2]);
+
+    // Figure out which direction the angle needs to
+    // be applied in.
+    var angle = oldVec.angleTo(newVec);
+    oldVec.applyAxisAngle(axis, angle);
+    var dist1 = oldVec.distanceTo(newVec);
+    oldVec.applyAxisAngle(axis, -2*angle);
+    var dist2 = oldVec.distanceTo(newVec);
+
+    if (dist1 < dist2) {
+      return angle;
+    } else {
+      return -angle;
+    }
   };
 
   window.addEventListener('load', function() {
